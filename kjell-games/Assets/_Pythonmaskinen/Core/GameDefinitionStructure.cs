a14c1746d1e5e4a74a6a7ac78047da020451bda9
Jonathan Bäckström <jonathan.backstrom95@gmail.com>
2018-06-27 10:12:07 +0200

    Import UI

---
+++ kjell-games/Assets/_Pythonmaskinen/Core/GameDefinitionStructure.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+
+public class ActiveLevel
+{
+	public string sceneName { get; set; }
+	public string levelId { get; set; }
+}
+
+public class SceneSettings
+{
+	public float walkerStepTime { get; set; }
+	public bool gameWindowUiLightTheme { get; set; }
+	public List<string> availableFunctions { get; set; }
+}
+
+public class GuideBubble
+{
+	public string target { get; set; }
+	public string text { get; set; }
+}
+
+public class TaskDescription{
+    public string header { get; set; }
+    public string body { get; set; }
+}
+
+public class LevelSettings
+{
+	public string precode { get; set; }
+	public string startCode { get; set; }
+	public int rowLimit { get; set; }
+    public TaskDescription taskDescription { get; set; }
+	public List<string> availableFunctions { get; set; }
+}
+
+/// <summary>
+/// Can be used to specify custom level definitions in json. 
+/// Should be implemented as partial class in file outside of _Pythonmaskinen.
+/// </summary>
+public partial class LevelDefinition
+{
+}
+
+public class CaseSettings
+{
+	public string precode { get; set; }
+	public float walkerStepTime { get; set; }
+}
+
+/// <summary>
+/// Can be used to specify custom case definitions in json. 
+/// Should be implemented as partial class in file outside of _Pythonmaskinen.
+/// </summary>
+public partial class CaseDefinition
+{
+}
+
+public class Case
+{
+	public CaseSettings caseSettings { get; set; }
+	public CaseDefinition caseDefinition { get; set; }
+}
+
+public class Level
+{
+	public string id { get; set; }
+	public List<GuideBubble> guideBubbles { get; set; }
+	public LevelSettings levelSettings { get; set; }
+	public LevelDefinition levelDefinition { get; set; }
+	public List<Case> cases { get; set; }
+}
+
+public class Scene
+{
+	public string name { get; set; }
+	public SceneSettings sceneSettings { get; set; }
+	public List<Level> levels { get; set; }
+}
+
+public class GameDefinition
+{
+	public List<ActiveLevel> activeLevels { get; set; }
+	public List<Scene> scenes { get; set; }
+}
